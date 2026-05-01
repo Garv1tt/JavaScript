d3fe413894b17ce7c91fd19dabf0fb121b6faa01
Garv1tt <garvitt.242@gmail.com>
2026-05-01 13:13:29 +0000

    datatypes

---
+++ JavaScript/datatypes.js
@@ -0,0 +1,10 @@
+"use strict";  //this lets all the JS code to run in newer version 
+
+
+// alert(17 + 3) this will show error as we are running the code in nodejs and not in the browser
+
+
+// console.log("Garvit"); console.log(4 + 4) but this is not good as it is not properly readable, thus the correct way is
+
+console.log(4 + 4)
+console.log("Garvit")
