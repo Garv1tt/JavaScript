26d0834513d75a72fec01845df4ef562d221f685
Garv1tt <garvitt.242@gmail.com>
2026-05-01 13:13:10 +0000

    variables done

---
+++ JavaScript/variables.js
@@ -0,0 +1,14 @@
+const AccountID = 12345
+let AccountName = "Garvit"
+let AccountEmail = "garvitt.27@gmail.com"
+let AccountPassword = "Hello@123"
+let AccountCity;
+
+// AccountName = "garvit"
+// AccountEmail = "garvitt.242@gmail.com"
+// AccountPassword = "hellothere"
+
+// we do not use var nowadays because of functonal and block scope
+// we can also assign values wihout using let also bu it is not a professional method
+
+console.table([AccountID, AccountName, AccountEmail, AccountPassword, AccountCity])
