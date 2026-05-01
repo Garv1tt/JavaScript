fbf1884b7217416500149b58842d46b64da28eec
Garv1tt <garvitt.242@gmail.com>
2026-05-01 19:23:09 +0000

    conversions done

---
+++ JavaScript/conversions.js
@@ -0,0 +1,28 @@
+score = "45abc"
+
+let number = Number(score)
+// console.log(typeof number);
+// console.log(number)
+// NaN means not a number
+// in booelean true means one and false means zero
+// null is zero and undefined is NaN
+// the problem is that in type of score it shows number whether it is true or not just lke in the case above
+// like above where we used Number as a function we can also use String,Booelean et and mostly al the datatypes 
+
+
+let isloggedin1 = 0
+let isloggedin2 = 1
+let isloggedin3 = ""
+let isloggedin4 = "Hello!"
+
+booleanloggedin1 = Boolean(isloggedin1)
+booleanloggedin2 = Boolean(isloggedin2)
+booleanloggedin3 = Boolean(isloggedin3)
+booleanloggedin4 = Boolean(isloggedin4)
+
+// console.table([booleanloggedin1, booleanloggedin2, booleanloggedin3, booleanloggedin4])
+
+let somenumber = 33
+stringnumber = String(somenumber)
+console.log(stringnumber);
+console.log(typeof stringnumber);
