1f3235c38a2f62a51347c9790b32fc206e01b0b1
Garv1tt <garvitt.242@gmail.com>
2026-05-02 23:24:21 +0000

    num and maths done

---
+++ JavaScript/num_and_maths.js
@@ -0,0 +1,34 @@
+// -----------------------------------Numbers--------------------------------
+const score = 400
+// console.log(score);
+const newscore = new Number(100)
+// console.log(newscore);
+// console.log(newscore.toString().length);
+// console.log(score.toFixed(2));
+const decimal = 123.976
+const bignumber = 100000000
+// console.log(decimal.toPrecision(3));
+// console.log(bignumber.toLocaleString('en-IN'))
+
+
+// -------------------------------------Maths--------------------------------
+
+
+// console.log(Math);
+// console.log(Math.abs(-4));
+// console.log(Math.round(4.6));
+// console.log(Math.ceil(4.2));
+// console.log(Math.floor(4.9));
+// console.log(Math.min(4, 3, 6, 8));
+// console.log(Math.max(4, 3, 6, 8));
+
+console.log(Math.random());
+console.log((Math.random()*10) + 1);
+console.log(Math.floor(Math.random()*10) + 1);
+
+const min = 10
+const max = 20
+
+console.log(Math.floor(Math.random() * (max - min + 1)) + min)
+// above line is very important 
+
