9d178c1ebcbd0e53c5ad1a9b401e027f7675cb82
Garv1tt <garvitt.242@gmail.com>
2026-05-02 18:04:41 +0000

    comparision done

---
+++ JavaScript/comparision.js
@@ -0,0 +1,19 @@
+// firstly they are some basic calculative operations such as <,>,>=,<=,!=etc which returns value in true or false for some simple mathematiic problems
+
+
+// also remember when comparing two or more objects their datattype must be same because it can prevent any logical error eg
+
+// console.log("2" > 1)
+// console.log("02" > 1)
+
+// console.log(null > 0);
+// console.log(null == 0);
+// console.log(null != 0)
+// console.log(null>= 0);
+// console.log(undefined > 0)
+
+// === it means if the content inside and the datatype is same then only it will return true else false
+
+// console.log("2" === 2);
+// console.log(2 === 2);
+
