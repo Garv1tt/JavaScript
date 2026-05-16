4d20e9a4ef48a643d47c4125431d9c8319392328
Garv1tt <garvitt.242@gmail.com>
2026-05-16 11:21:54 +0000

    callstack done

---
+++ JavaScript/callstack.js
@@ -0,0 +1,6 @@
+// JavaScript Execution Contest
+// 1. Global EC
+// 2. Functional EC
+// 3. Eval EC(we do not study much abt it)
+// the execution takes place in two phases memory and execution 
+// the rest of the video was explained https://youtu.be/ByhtOgF6uYM?si=f3fpxF_26ZWDJkJ0
