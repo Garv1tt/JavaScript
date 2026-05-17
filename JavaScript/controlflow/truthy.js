dd184681dff95de77e2af50b9b3dba41c2164ac0
Garv1tt <garvitt.242@gmail.com>
2026-05-17 20:48:08 +0000

    truthy and falsy values done

---
+++ JavaScript/controlflow/truthy.js
@@ -0,0 +1,31 @@
+// let useremail = []
+// if (useremail){
+//     console.log("got the userermail");
+    
+// } else{
+//     console.log("dont have an email");
+    
+// }
+// when we leave an empty string and run the code it gives dont have an email but when we do the same with {} and [] we get got the username that is why because they are truthy value and string is an falsy value.    
+
+// falsy values--> false,0,-0,bigint 0n, "",null,nan,undefined
+// truthy values-->true,1,[],{}," ","0"(important),'false'(important),function(){} 
+// how to check if the array and object is emoty or not 
+// const name = []
+// if(name.length ===0){
+//     console.log("Array is empty");
+    
+// }
+// else{
+//     console.log("got the name");
+    
+// }
+// const surname = {}
+// if (Object.keys(surname).length ===0){
+//     console.log("Object is empty");
+    
+// }
+// else{
+//     console.log("got the surname");
+    
+// }
