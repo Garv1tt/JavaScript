0729cdebaa38cac90436077f989bc3f2d619ec6a
Garvitt <garvitt.242@gmail.com>
2026-07-13 17:48:42 +0000

    getter setter through classes done

--- JavaScript/getset.js
+++ JavaScript/getset.js
@@ -1,10 +1,23 @@
 //  Getter and Setter and Stack Overflow
-
+// (Modern Way and is the nicest among the 3 in sintactical clarity also)
 class User{
     constructor(email, password){
         this.email = email;
         this.password = password
     }
+    get password(){
+        return this._password.toUpperCase()
+    }
+    set password(value){
+        this._password = value
+    }
+    get email(){
+        return this._email.toUpperCase()
+    }
+    set email(value){
+        this._email = value
+    }
 }
-const test = new User("g@gmail.com", "2622009")
-console.log(test.password)
+const test = new User("g@gmail.com", "garvit")
+console.log(test.email)
+
