43c9bbd7e065b5b3116247006adb30eff057c856
Garvitt <garvitt.242@gmail.com>
2026-07-13 15:38:21 +0000

    to be continued

--- JavaScript/getset.js
+++ JavaScript/getset.js
@@ -1,1 +1,10 @@
-console.log("Hello, World!")
+//  Getter and Setter and Stack Overflow
+
+class User{
+    constructor(email, password){
+        this.email = email;
+        this.password = password
+    }
+}
+const test = new User("g@gmail.com", "2622009")
+console.log(test.password)
