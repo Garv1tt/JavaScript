1ce56aa1a98e89a4c631b4182a3eaad2dd74d174
Garvitt <garvitt.242@gmail.com>
2026-08-18 14:44:30 +0000

    Project-4(Number Guessing Game)

--- JavaScript/Projects/Project4/Project4.js
+++ JavaScript/Projects/Project4/Project4.js
@@ -30,7 +30,7 @@ function validateGuess(guess) {
   } else if (guess < 1) {
     alert('PLease enter a number more than 1');
   } else if (guess > 100) {
-    alert('PLease enter a  number less than 100');
+    alert('Please enter a  number less than 100');
   } else {
     prevGuess.push(guess);
     if (numGuess === 11) {
